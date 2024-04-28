570448cd8f70bf2249d37efdebdbdae06443b09c
crumblingstatue <radiantstatue@gmail.com>
2024-04-28 23:37:38 +0200

    Move markdown viewer max width a bit closer to the sidebar

--- src/ui.rs
+++ src/ui.rs
@@ -634,7 +634,7 @@ fn package_list_ui(project: &Project, ui: &mut egui::Ui, gui: &mut Gui) {
 fn markdown_ui(ui: &mut egui::Ui, gui: &mut Gui, project: &Project) {
     central_top_bar(ui, gui, project);
     egui::ScrollArea::vertical().show(ui, |ui| {
-        ui.set_max_width(gui.right_panel_left - 16.0);
+        ui.set_max_width(gui.right_panel_left - 14.0);
         if gui.style.name == "crates.io" {
             // Hack to make things more legible
             ui.style_mut().visuals = egui::Visuals::light();
